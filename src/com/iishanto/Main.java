69172a310c029264804df46eddf6def30c2c80a2
Sharif Hasan <me.sharif.hasan@gmail.com>
2024-06-28 18:24:28 +0600

    conflict solved, git do not consider language sementics

--- src/com/iishanto/Main.java
+++ src/com/iishanto/Main.java
@@ -12,10 +12,6 @@ public class Main {
     public static void main(String[] args) throws Exception{
         UserManagerSingleton.getInstance().loadUsers();
         TimeUnit.MILLISECONDS.sleep(1000);
-        int x=5;
-
-
-        int x=5;
 
     }
 }
